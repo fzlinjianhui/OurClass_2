d3004ea7a1e7f880325a1a41da26b406cc7da7b7
JianHui_Lin <602003139@qq.com>
2016-03-16 23:11:32 +0800

    test

--- src/cn/shawadika/servlet/Servlet.java
+++ src/cn/shawadika/servlet/Servlet.java
@@ -4,10 +4,4 @@ public class Servlet {
 	public static void main(String[] args) {
 		System.out.println("test");
 	}
-	public void test(){
-		System.out.println("test");
-	}
-	public test2(){
-		
-	}
 }
